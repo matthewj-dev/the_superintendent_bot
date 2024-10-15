fdc8b41c3bb6a7d4278905171e10c273a8a8a99a
Matthew Jackson <13512861+matthewj-dev@users.noreply.github.com>
2024-10-15 17:44:54 -0500

    add some logging to main.ts

--- main.ts
+++ main.ts
@@ -6,12 +6,16 @@ const shouldUseWebhook =
   Deno.env.get(USE_WEBHOOK)?.trim().toLowerCase() === "true"
 
 if (shouldUseWebhook === false) {
+  console.info("Using Long Polling")
   bot.start()
 }
+
+console.info("Using Webhook")
 
 const handleUpdate = webhookCallback(bot, "std/http")
 
 Deno.serve(async (req) => {
+  console.info(req)
   if (req.method === "POST") {
     const url = new URL(req.url)
     if (url.pathname.slice(1) === bot.token) {
